9fd118eaac0b6ff8803216a22c046653e5420006
serby100 <serby1@groundk.com>
2025-10-30 10:23:12 +0900

    fix: Set base path to root for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,14 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const isProduction = mode === 'production';
+export default defineConfig(() => {
     return {
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
-      base: isProduction ? '/NovelToIllust/' : '/',
+      base: '/',
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
